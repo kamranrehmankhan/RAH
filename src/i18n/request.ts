8b83e6c1e0214a8bdbd19af78ed81f075aee2d78
kamranrehmankhan <kamranrehmanpk@gmail.com>
2026-03-21 23:05:17 +0500

    Fix next-intl v4 requestLocale API

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,8 +1,11 @@
 import {getRequestConfig} from 'next-intl/server'
+import {routing} from './routing'
 
-export default getRequestConfig(async ({locale}) => {
-  const safeLocale = locale ?? 'en'
-  const messages = (await import(`../../messages/${safeLocale}.json`)).default
-  return {locale: safeLocale, messages}
+export default getRequestConfig(async ({requestLocale}) => {
+  let locale = await requestLocale
+  if (!locale || !routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale
+  }
+  const messages = (await import(`../../messages/${locale}.json`)).default
+  return {locale, messages}
 })
-
